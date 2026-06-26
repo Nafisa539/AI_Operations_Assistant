85d33feac8cd6ca1ddb92afeb7c346f361f5dc29
Nafisa539 <202317b3079@wilp.bits-pilani.ac.in>
2026-06-26 15:56:21 +0000

    updated files

--- Backened/app.py
+++ Backened/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from chatbot import get_response
  
-app = Flask(__name__)
+app = Flask(_name_)
 CORS(app)
  
 @app.route("/")
@@ -20,5 +20,5 @@ def chat():
  
     return jsonify({"response": answer})
  
-if __name__ == "__main__":
+if _name_ == "_main_":
     app.run(debug=True)
