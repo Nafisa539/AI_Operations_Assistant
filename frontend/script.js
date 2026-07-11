7d684ea07699364b16b41704493b5521c40a0ab9
Nafisa539 <202317b3079@wilp.bits-pilani.ac.in>
2026-07-11 05:36:24 +0000

    ok

---
+++ frontend/script.js
@@ -0,0 +1,36 @@
+async function sendQuestion() {
+ 
+    let question = document.getElementById("question").value;
+ 
+    if (question == "") {
+        return;
+    }
+ 
+    let chatBox = document.getElementById("chat-box");
+ 
+    chatBox.innerHTML +=
+        "<div class='user'><span>" + question + "</span></div>";
+ 
+    document.getElementById("question").value = "";
+ 
+    let response = await fetch("https://urban-space-succotash-wvrw6pgjvvx6h57qv-5000.app.github.dev/chat", {
+ 
+        method: "POST",
+ 
+        headers: {
+            "Content-Type": "application/json"
+        },
+ 
+        body: JSON.stringify({
+            message: question
+        })
+ 
+    });
+ 
+    let data = await response.json();
+ 
+    chatBox.innerHTML +=
+        "<div class='bot'><span>" + data.response + "</span></div>";
+ 
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
